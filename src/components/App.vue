9d443bb4bfaf59ecdff1859bddd3dd44d2095163
somerq <somerq@qq.com>
2018-01-12 11:31:32 +0800

    Shopping Mall项目脚手架提交

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+        <header></header>
+        <app-login></app-login>
+        <app-register></app-register>
+        <footer></footer>
+
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+
+</style>
