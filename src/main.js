ac8b1ac05411316455a5dd8a5d4ff8da5b6a79a9
somerq <somerq@qq.com>
2018-01-13 10:01:44 +0800

    '集成ElementUI并测试组件'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import VueRouter from 'vue-router';
 Vue.use(VueRouter);
+Vue.use(ElementUI);
 import App from './components/App.vue';
 import routerConfig from './router/index.js';
 import axios from 'axios';
